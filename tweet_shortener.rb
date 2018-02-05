97f1bdf805fa914247a7525c5041cc72f75d0156
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-05 18:24:04 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def dictionary(lookup)
+dict = {"hello" => 'hi',
+"to"  => '2',
+"two"  => '2', 
+"too" => '2',
+"for" => '4',
+"four" => '4',
+'be' becomes 'b',
+'you' becomes 'u',
+"at" becomes "@",
+"and" becomes "&"}
+if dict[lookup] != nil
+  dict[lookup]
+end
+def word_substituter(string)
+  
+end
+to_sym
