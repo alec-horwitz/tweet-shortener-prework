31db4cd76af7012b652172154997161d2d4944c4
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-05 18:36:09 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,20 +1,25 @@
 # Write your code here.
 def lookup(word)
   dict = {
-    "hello" => "hi", 
-    "to"  => "2", 
-    "two"  => "2", 
-    "too" => "2", 
-    "for" => "4", 
-    "four" => "4", 
-    "be" => "b", 
-    "you" => "u", 
-    "at" => "@", 
+    "hello" => "hi",
+    "to"  => "2",
+    "two"  => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
     "and" => "&"
   }
   dict[word]
 end
 def word_substituter(string)
-
+  newString = string.split(" ")
+  newString.each {|item|
+    replacement = lookup(item)
+    if replacement != nil
+      item = replacement
+    end
+  }
 end
-to_sym
