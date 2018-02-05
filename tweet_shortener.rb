3652c3446f5a82b2455f361902a04e734ae57233
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-05 18:29:43 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,18 @@
 # Write your code here.
 def lookup(word)
-dict = {"hello" => "hi", "to"  => "2", "two"  => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
-dict[word]
+  dict = {
+    "hello" => "hi", 
+    "to"  => "2", 
+    "two"  => "2", 
+    "too" => "2", 
+    "for" => "4", 
+    "four" => "4", 
+    "be" => "b", 
+    "you" => "u", 
+    "at" => "@", 
+    "and" => "&"
+  }
+  dict[word]
 end
 def word_substituter(string)
 
